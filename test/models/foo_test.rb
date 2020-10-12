7d5fb484347d0e767d6a2c259b65c65998bc351c
Ken Collins <ken@metaskills.net>
2020-10-12 07:51:30 -0400

    Add a few tests.

---
+++ test/models/foo_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class FooTest < ActiveSupport::TestCase
+  def test_something
+    assert_equal true, true
+  end
+
+  def test_something2
+    assert_equal true, true
+  end
+end
